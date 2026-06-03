480e2c35fa8a1f5d05c89000b1a1c817964aaa52
Chandru Shree <chandrushree@Mac.lan>
2026-06-03 10:35:26 +0530

    Assignments

---
+++ GSITAssignment/src/assignmentLooping/printnumbers.java
@@ -0,0 +1,23 @@
+package assignmentLooping;
+
+import java.util.Scanner;
+
+public class printnumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		 System.out.print("Enter a number: ");
+	     int num = sc.nextInt();
+	     
+	     
+	     for(int i = 1; i <= num; i ++) {
+	    	 
+	    	 System.out.println(i);
+	     }
+		
+	     sc.close();
+	}
+
+}
